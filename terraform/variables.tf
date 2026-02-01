255a7b4e1a156229aadefaf104311f9af96417e1
balarithwik <rithwik10122000@gmail.com>
2026-02-01 23:32:03 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,14 @@
-variable "client_id" {
-  type      = string
-  sensitive = true
-}
-
-variable "client_secret" {
-  type      = string
-  sensitive = true
-}
-
-variable "tenant_id" {
-  type      = string
-  sensitive = true
-}
-
-variable "subscription_id" {
-  type = string
-}
+# -------------------------------
+# SSH Keys (used by VM provisioning)
+# -------------------------------
 
 variable "ssh_public_key" {
-  type      = string
-  sensitive = true
+  description = "SSH public key for Azure VM access"
+  type        = string
 }
 
 variable "ssh_private_key" {
-  type      = string
-  sensitive = true
+  description = "SSH private key for Azure VM access"
+  type        = string
+  sensitive   = true
 }
