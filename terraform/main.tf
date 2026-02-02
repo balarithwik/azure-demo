f475fb455eb21cada17e9f8b5161ef8587f28871
balarithwik <rithwik10122000@gmail.com>
2026-02-02 10:43:04 +0530

    Update10 main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,36 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.64.0"
+      version = "~> 3.100"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
 }
 
-# -------------------------------
+############################
 # RESOURCE GROUP
-# -------------------------------
+############################
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-demo"
   location = "West US 2"
 }
 
-# -------------------------------
-# NETWORKING
-# -------------------------------
+############################
+# NETWORK
+############################
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "demo-vnet"
-  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
@@ -37,9 +40,25 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# -------------------------------
+############################
+# NSG
+############################
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "demo-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+############################
 # PUBLIC IP (STANDARD SKU)
-# -------------------------------
+############################
+
 resource "azurerm_public_ip" "vm_ip" {
   name                = "demo-vm-ip"
   location            = azurerm_resource_group.rg.location
@@ -49,67 +68,34 @@ resource "azurerm_public_ip" "vm_ip" {
   sku               = "Standard"
 }
 
+############################
+# NETWORK INTERFACE
+############################
+
 resource "azurerm_network_interface" "nic" {
   name                = "demo-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "primary"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.vm_ip.id
   }
 }
 
-# -------------------------------
-# NETWORK SECURITY GROUP
-# -------------------------------
-resource "azurerm_network_security_group" "nsg" {
-  name                = "demo-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "NodePort"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "30000-32767"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "assoc" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# -------------------------------
+############################
 # LINUX VM
-# -------------------------------
+############################
+
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "demo-vm"
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_D4s_v3"
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
 
-  admin_username = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
@@ -121,37 +107,21 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts-gen2"
+    sku       = "20_04-lts"
     version   = "latest"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install -y docker.io",
-      "sudo systemctl enable docker",
-      "sudo systemctl start docker"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "azureuser"
-      private_key = var.ssh_private_key
-      host        = azurerm_public_ip.vm_ip.ip_address
-      timeout     = "20m"
-    }
   }
 }
 
-# -------------------------------
-# OUTPUTS
-# -------------------------------
+############################
+# OUTPUT
+############################
+
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_ip.ip_address
 }
