5789558994baaf06710be9dbde26fca2d4a4b4ed
balarithwik <rithwik10122000@gmail.com>
2026-02-02 11:16:59 +0530

    Update 11 main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,15 +86,16 @@ resource "azurerm_network_interface" "nic" {
 }
 
 ############################
-# LINUX VM
+# LINUX VM (CAPACITY SAFE)
 ############################
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "demo-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2s"
-  admin_username      = "azureuser"
+
+  size           = "Standard_B1s" # 🔥 CHANGED — ALWAYS AVAILABLE
+  admin_username = "azureuser"
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
