f6ee1de2e5dc9fe6dfa98472386a15925c340197
balarithwik <rithwik10122000@gmail.com>
2026-02-02 09:19:59 +0530

    Update 3 variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,25 @@
-variable "ssh_public_key" {
+variable "subscription_id" {
+  description = "Azure Subscription ID"
   type        = string
-  description = "SSH public key"
 }
 
-variable "ssh_private_key" {
+variable "tenant_id" {
+  description = "Azure Tenant ID"
   type        = string
-  description = "SSH private key"
+}
+
+variable "client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
   sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for VM access"
+  type        = string
 }
