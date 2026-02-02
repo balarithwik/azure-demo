6ec07f3c91a420cf1c7267c0a921b8ec80f6fedd
balarithwik <rithwik10122000@gmail.com>
2026-02-02 10:20:18 +0530

    Update 5 variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,10 @@
 variable "ssh_public_key" {
-  type = string
+  description = "SSH public key for VM login"
+  type        = string
+}
+
+variable "ssh_private_key" {
+  description = "SSH private key for VM provisioning"
+  type        = string
+  sensitive   = true
 }
