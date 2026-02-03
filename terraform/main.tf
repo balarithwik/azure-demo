54084f3d087de547c2f622dffaf61fdff8d6495e
balarithwik <rithwik10122000@gmail.com>
2026-02-04 01:29:44 +0530

    Update 22 main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,49 +10,65 @@ terraform {
 }
 
 ############################
-# PROVIDER (SERVICE PRINCIPAL AUTH)
+# PROVIDER (GitHub Actions)
 ############################
 
 provider "azurerm" {
   features {}
 
-  skip_provider_registration = true
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 ############################
-# RESOURCE GROUP (EXISTING)
+# RESOURCE GROUP
 ############################
 
-data "azurerm_resource_group" "rg" {
-  name = "rg-terraform-demo"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-demo"
+  location = "westus2"
 }
 
 ############################
-# NETWORK
+# VIRTUAL NETWORK
 ############################
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "demo-vnet"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "demo-subnet"
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 ############################
-# NSG
+# NETWORK SECURITY GROUP
 ############################
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "demo-nsg"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
@@ -61,13 +77,13 @@ resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
 }
 
 ############################
-# PUBLIC IP
+# PUBLIC IP (STANDARD SKU)
 ############################
 
 resource "azurerm_public_ip" "vm_ip" {
   name                = "demo-vm-ip"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   allocation_method = "Static"
   sku               = "Standard"
@@ -79,8 +95,8 @@ resource "azurerm_public_ip" "vm_ip" {
 
 resource "azurerm_network_interface" "nic" {
   name                = "demo-nic"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -96,15 +112,17 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "demo-vm"
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
 
-  size           = "Standard_B1s"
   admin_username = "azureuser"
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
+
+  disable_password_authentication = true
 
   admin_ssh_key {
     username   = "azureuser"
