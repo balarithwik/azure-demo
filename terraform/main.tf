000d585663d29eb826ca5438beb870b263e167eb
balarithwik <rithwik10122000@gmail.com>
2026-02-04 09:54:36 +0530

    Update 24 main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,8 +110,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "demo-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-
+  size                = "Standard_D4s_v3"
   admin_username = "azureuser"
 
   network_interface_ids = [
